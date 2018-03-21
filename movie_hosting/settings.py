b5bb03d3e02556ccd581707ff9d2c8c323fc2db0
sinzee <34213071+sinzee@users.noreply.github.com>
2018-03-21 17:33:11 +0900

    change language code and time zone

--- movie_hosting/settings.py
+++ movie_hosting/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
