1723ea00803c51d9f8ebb9eb14e821b78561fded
sinzee <admin@example.com>
2018-12-06 05:08:49 +0900

    add user info to siteuser one when using rest api

--- movie/api/serializers.py
+++ movie/api/serializers.py
@@ -1,9 +1,28 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from ..models import SiteUser
 
 
-class SiteUserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+                    'id',
+                    'username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                 )
+
+
+class SiteUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = SiteUser
-        fields = ('url', 'bio', )
+        fields = (
+                    'url',
+                    'bio',
+                    'user',
+                 )
--- movie/tests/api/test_views.py
+++ movie/tests/api/test_views.py
@@ -17,17 +17,20 @@ class RestApiTest(TestCase):
     def setUpTestData(cls):
         cls.url_path = '/api/v1/user/'
 
-        mail_address = 'test@example.com'
-        test_user = User.objects.create_user(
-                        username=mail_address,
-                        password='12345',
-                        email=mail_address,
-                        first_name='Super',
-                        last_name='John',
-                    )
+        cls.mail_address = 'test@example.com'
+        cls.first_name = 'Super'
+        cls.last_name = 'John'
+        cls.test_user = User.objects.create_user(
+                            username=cls.mail_address,
+                            password='12345',
+                            email=cls.mail_address,
+                            first_name=cls.first_name,
+                            last_name=cls.last_name,
+                        )
+        cls.bio = 'user bio'
         cls.site_user = SiteUser.objects.create(
-                            user=test_user,
-                            bio='user bio'
+                            user=cls.test_user,
+                            bio=cls.bio
                         )
 
     def test_view_url_exists_at_desired_location(self):
@@ -42,31 +45,18 @@ class RestApiTest(TestCase):
                                             path=self.url_path,
                                             pk=self.site_user.pk
                                        ),
-                                'bio': 'user bio',
+                                'bio': self.bio,
+                                'user': {
+                                    'id': self.test_user.pk,
+                                    'username': self.mail_address,
+                                    'email': self.mail_address,
+                                    'first_name': self.first_name,
+                                    'last_name': self.last_name,
+                                },
                             },
                         ]
         self.assertEqual(
             json.loads(resp.content),
             expected_data
         )
-
-    def test_api_gets_user_detail(self):
-        resp = self.client.get(
-                '{path}{pk}/'.format(
-                    path=self.url_path,
-                    pk=self.site_user.pk
-                )
-               )
-        expected_data = {
-                            'url': 'http://testserver{path}{pk}/'.format(
-                                        path=self.url_path,
-                                        pk=self.site_user.pk
-                                   ),
-                            'bio': 'user bio',
-                        }
-        self.assertEqual(
-            json.loads(resp.content),
-            expected_data
-        )
-
 
